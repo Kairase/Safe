04737b12c948087884f79aaa72b3132da34115ac
Kairase <70236704+Kairase@users.noreply.github.com>
2020-08-27 21:17:47 +0400

    v1.0

---
+++ Safe/Safe.py
@@ -0,0 +1,30 @@
+from random import randint
+
+password = ""
+
+def set_password():
+	pasw = randint(0, 9999)
+
+	for i in range(0, 4 - len(str(pasw))):
+		pasw = "0" + str(pasw)
+
+	return str(pasw)
+
+def main():
+	global password
+	password = str(set_password())
+
+	if enter_password() == True:
+		print("Поздравляю, у тебя получилось взломать программу! Ты крутой праграммист!")
+		while True:
+			pass
+
+def enter_password():
+	inp = str(input("Введите пароль: "))
+
+	if inp == password:
+		return True
+	else:
+		enter_password()
+
+main()
